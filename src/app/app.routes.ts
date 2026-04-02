9c0ee6f296f29cfac16475edecf438e8ba2bb898
rohitjha424 <rohitjha424@gmail.com>
2026-04-02 21:03:00 +0530

    routes setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from '../app/pages/home/home';
+import { About } from '../app/pages/about/about';
+import { Projects } from '../app/pages/projects/projects';
+import { Services } from '../app/pages/services/services';
+import { Contact } from '../app/pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'services', component: Services },
+  { path: 'contact', component: Contact }
+];
