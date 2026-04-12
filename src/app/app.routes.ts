2e523b79f524c8c1491761558736049a8fc10dc5
rohitjha424 <rohitjha424@gmail.com>
2026-04-12 10:20:00 +0530

    single page scroll route implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,6 @@
-import { Component } from '@angular/core';
-import { Navbar } from './components/navbar/navbar';
-import { Footer } from './components/footer/footer';
+import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
- 
-@Component({
-  selector: 'app-root',
-  imports: [Navbar, Footer, Home],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {}
- 
+
+export const routes: Routes = [
+  { path: '**', component: Home }
+];
